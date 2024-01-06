ddb92161bec0dbdcacfb5c655d55c5f7d7ab39f4
rookie-leo <proleonardopa@gmail.com>
2024-01-06 14:02:37 -0300

    [ Build ]Configured producer for JSON objects

--- src/main/java/br/com/paymentservice/services/impl/PaymentServiceImpl.java
+++ src/main/java/br/com/paymentservice/services/impl/PaymentServiceImpl.java
@@ -1,15 +1,27 @@
 package br.com.paymentservice.services.impl;
 
 import br.com.paymentservice.services.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import br.com.paymentservice.models.Payment;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
         log.info("Payment {} received", payment);
+        Thread.sleep(1000);
+        log.info("Send message to topic");
+        kafkaTemplate.send("payments-topic", payment);
     }
 }
